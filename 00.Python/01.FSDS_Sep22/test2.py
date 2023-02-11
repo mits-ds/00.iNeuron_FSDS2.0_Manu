1191d6331969312672c8bbdf6dea27a2452da2dd
Manu <manumadhav@gmail.com>
2023-02-11 14:11:09 +0530

    _

---
+++ 00.Python/01.FSDS_Sep22/test2.py
@@ -0,0 +1,14 @@
+def sum_(a,b):
+    return a+b
+
+def sub_(a,b):
+    return a-b
+
+def mult_(a,b):
+    if b>0:
+        return a/b
+    else:
+        return " Division by Zero"
+
+def div_(a,b):
+    return a*b
